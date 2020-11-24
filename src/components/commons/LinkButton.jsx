9e393963d639490827918705ed73f9e5067e0566
rabbykhairul <rabbykhairul@gmail.com>
2020-11-24 12:53:06 +0600

    LinkButton - apply text color dynamically based on props input

--- src/components/commons/LinkButton.jsx
+++ src/components/commons/LinkButton.jsx
@@ -17,7 +17,7 @@ const getStyles = (styleInputs) => {
   return {
     fontSize: styleInputs.size === "sm" ? "0.7vw" : "0.9vw",
     fontWeight: "500",
-    color: "rgb(8,8,8)",
+    color: styleInputs.color ? styleInputs.color : "rgb(8,8,8)",
     letterSpacing: "0.025vw",
     padding: styleInputs.size === "sm" ? "0.6vw 2.15vw" : "0.75vw 2.35vw",
     border: "0.1vw solid",
