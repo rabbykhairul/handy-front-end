fa497f9b64c9c32a83fb246f4219ebf9d74779c3
rabbykhairul <rabbykhairul@gmail.com>
2020-11-24 14:16:35 +0600

    remove unused reference to Link component of react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Link } from "react-router-dom";
+import { Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import UtilServicesPage from "./components/utilServices/utilServicesPage";
 
