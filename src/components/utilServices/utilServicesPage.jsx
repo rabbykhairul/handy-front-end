2e9e18c3eba863f3a22cbde6b7a75a64cb854717
rabbykhairul <rabbykhairul@gmail.com>
2020-11-28 16:42:19 +0600

    add nested routing in utilServicesPage for resize image service

--- src/components/utilServices/utilServicesPage.jsx
+++ src/components/utilServices/utilServicesPage.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import TopNavBar from "../commons/TopNavBar";
+import ResizeImageSection from "./resizeImage/ResizeImageSection";
+import "./utilServicesPage.css";
 
-const UtilServicesPage = () => {
-  return <TopNavBar pageType="util-services" />;
+const UtilServicesPage = (props) => {
+  return (
+    <div className="util-services-page">
+      <TopNavBar pageType="util-services" />
+      {renderSelectedServiceSection(props)}
+    </div>
+  );
 };
 
 export default UtilServicesPage;
+
+const renderSelectedServiceSection = (props) => {
+  return (
+    <Switch>
+      <Route
+        path="/util-services/resize-image"
+        component={ResizeImageSection}
+      />
+    </Switch>
+  );
+};
