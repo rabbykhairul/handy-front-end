0c61847b2a740aed3fdfcbee9f34fbf12973dc7f
rabbykhairul <rabbykhairul@gmail.com>
2020-11-29 18:49:08 +0600

    fix bug - result var empty upon successful server response

--- src/services/imageService.js
+++ src/services/imageService.js
@@ -5,7 +5,8 @@ const API_END_POINT = "http://localhost:3000/api/resizeImage";
 const getResizedImage = async (userInput) => {
   const data = new FormData();
   for (let key in userInput) data.append(key, userInput[key]);
-  const { result } = await http.post(API_END_POINT, data);
+  const { data: result } = await http.post(API_END_POINT, data);
+  console.log(result);
   return result;
 };
 
