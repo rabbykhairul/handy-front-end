e36d09760d2d0ce872b06e1fa7124212e6ac872a
rabbykhairul <rabbykhairul@gmail.com>
2020-11-29 16:29:05 +0600

    create FormButton component for forms

---
+++ src/components/commons/FormButton.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./FormButton.css";
+
+const FormButton = (props) => {
+  const { label } = props;
+
+  return <button className="form-button">Resize image</button>;
+};
+
+export default FormButton;
