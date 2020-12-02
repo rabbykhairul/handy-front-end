c48cbea2076eb83d89aeb65e908a71808927c81d
rabbykhairul <rabbykhairul@gmail.com>
2020-12-02 12:48:16 +0600

    create PomodoroSection component

---
+++ src/components/utilServices/pomodoro/PomodoroSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./PomodoroSection.css";
+
+const PomodoroSection = () => {
+  return (
+    <div className="service-section pomodoro-section">
+      <div className="card-style-content-area"></div>
+    </div>
+  );
+};
+
+export default PomodoroSection;
