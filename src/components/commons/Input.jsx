368b4517aafd2b00dc4d11cfff6a77e0895afadd
rabbykhairul <rabbykhairul@gmail.com>
2020-12-04 12:54:44 +0600

    add placeholder attribute to Input component

--- src/components/commons/Input.jsx
+++ src/components/commons/Input.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import "./Input.css";
 
 const Input = (props) => {
-  const { type, label, name, value, onChange } = props;
+  const { type, label, name, value, placeholder = "", onChange } = props;
 
   return (
     <label className="regular-input">
       <span>{label}</span>
-      <input type={type} name={name} value={value} onChange={onChange} />
+      <input
+        type={type}
+        name={name}
+        value={value}
+        placeholder={placeholder}
+        onChange={onChange}
+      />
     </label>
   );
 };
