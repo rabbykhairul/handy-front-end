a3b87cd5cfac9f4e2992ffbf731635d8feb40079
rabbykhairul <rabbykhairul@gmail.com>
2020-12-06 00:13:34 +0600

    render pomodoro service nav bar using CardStyleNavBar component

--- src/components/utilServices/pomodoro/PomodoroNavBar.jsx
+++ src/components/utilServices/pomodoro/PomodoroNavBar.jsx
@@ -1,51 +1,6 @@
 import React from "react";
-import Button from "../../commons/Button";
-import "./PomodoroNavBar.css";
+import CardStyleNavBar from "../../commons/CardStyleNavBar";
 
-const PomodoroNavBar = (props) => {
-  const {
-    leftSideButtons,
-    rightSideButtons,
-    activeButton,
-    onButtonClick,
-  } = props;
-
-  const renderButton = (button) => {
-    const selectStatus = button.value === activeButton.value;
-
-    return (
-      <div key={button.value}>
-        <Button
-          label={button.label}
-          selectStatus={selectStatus}
-          onClick={() => onButtonClick(button)}
-        />
-      </div>
-    );
-  };
-
-  const renderNavBarLeftSide = () => {
-    return (
-      <div className="pomodoro-nav-bar-left-side">
-        {leftSideButtons.map((button) => renderButton(button))}
-      </div>
-    );
-  };
-
-  const renderNavBarRightSide = () => {
-    return (
-      <div className="pomodoro-nav-bar-right-side">
-        {rightSideButtons.map((button) => renderButton(button))}
-      </div>
-    );
-  };
-
-  return (
-    <div className="pomodoro-nav-bar">
-      {renderNavBarLeftSide()}
-      {renderNavBarRightSide()}
-    </div>
-  );
-};
+const PomodoroNavBar = (props) => <CardStyleNavBar {...props} />;
 
 export default PomodoroNavBar;
