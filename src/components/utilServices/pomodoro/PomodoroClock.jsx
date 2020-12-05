17e5b6003df14cea359df3a8c76a69ca5f84cf93
rabbykhairul <rabbykhairul@gmail.com>
2020-12-06 01:46:24 +0600

    render ClockDisplay component inside PomodoroClock

--- src/components/utilServices/pomodoro/PomodoroClock.jsx
+++ src/components/utilServices/pomodoro/PomodoroClock.jsx
@@ -1,44 +1,8 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlayCircle, faPauseCircle } from "@fortawesome/free-solid-svg-icons";
-import "./PomodoroClock.css";
+import ClockDisplay from "../../commons/ClockDisplay";
 
 const PomodoroClock = (props) => {
-  const { milliSeconds, runningStatus, onControlClick } = props;
-
-  let seconds = Math.floor(Number(milliSeconds) / 1000);
-  let minutes = Math.floor(seconds / 60);
-  seconds = seconds - minutes * 60;
-
-  const renderTimeDisplay = () => {
-    const minutesString = `0${minutes}`.slice(-2);
-    const secondsString = `0${seconds}`.slice(-2);
-
-    return (
-      <div className="time-display">{`${minutesString}:${secondsString}`}</div>
-    );
-  };
-
-  const renderPlayButton = () => {
-    const playIcon = runningStatus ? faPauseCircle : faPlayCircle;
-
-    return (
-      <div className="clock-control">
-        <FontAwesomeIcon
-          icon={playIcon}
-          onClick={onControlClick}
-          style={{ cursor: "pointer" }}
-        />
-      </div>
-    );
-  };
-
-  return (
-    <div className="pomodoro-clock">
-      {renderTimeDisplay()}
-      {renderPlayButton()}
-    </div>
-  );
+  return <ClockDisplay {...props} />;
 };
 
 export default PomodoroClock;
