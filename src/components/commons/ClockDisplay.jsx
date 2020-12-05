b9146755fe9bd2ecfa547af17b0bcdb231ff8557
rabbykhairul <rabbykhairul@gmail.com>
2020-12-06 01:44:27 +0600

    extract a resuable ClockDisplay component from PomodoroClock

---
+++ src/components/commons/ClockDisplay.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlayCircle, faPauseCircle } from "@fortawesome/free-solid-svg-icons";
+import "./ClockDisplay.css";
+
+const ClockDisplay = (props) => {
+  const { milliSeconds, runningStatus, onControlClick } = props;
+
+  let seconds = Math.floor(Number(milliSeconds) / 1000);
+  let minutes = Math.floor(seconds / 60);
+  seconds = seconds - minutes * 60;
+
+  const renderTimeDisplay = () => {
+    const minutesString = `0${minutes}`.slice(-2);
+    const secondsString = `0${seconds}`.slice(-2);
+
+    return (
+      <div className="time-display">{`${minutesString}:${secondsString}`}</div>
+    );
+  };
+
+  const renderPlayButton = () => {
+    const playIcon = runningStatus ? faPauseCircle : faPlayCircle;
+
+    return (
+      <div className="clock-control">
+        <FontAwesomeIcon
+          icon={playIcon}
+          onClick={onControlClick}
+          style={{ cursor: "pointer" }}
+        />
+      </div>
+    );
+  };
+
+  return (
+    <div className="clock-display">
+      {renderTimeDisplay()}
+      {renderPlayButton()}
+    </div>
+  );
+};
+
+export default ClockDisplay;
