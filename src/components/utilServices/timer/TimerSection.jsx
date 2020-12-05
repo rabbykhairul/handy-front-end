c9ba2a94f3c79f63e44457906667dc70ff16f569
rabbykhairul <rabbykhairul@gmail.com>
2020-12-05 23:47:22 +0600

    create TimerSection component

---
+++ src/components/utilServices/timer/TimerSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TimerSection.css";
+
+const TimerSection = () => {
+  return (
+    <div className="service-section timer-section">
+      <div className="card-style-content-area"></div>
+    </div>
+  );
+};
+
+export default TimerSection;
