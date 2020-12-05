c24d8579b55510857221a14867603498a0b6d2b3
rabbykhairul <rabbykhairul@gmail.com>
2020-12-06 00:31:26 +0600

    render nav bar for timer utility service

---
+++ src/components/utilServices/timer/TimerNavBar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import CardStyleNavBar from "../../commons/CardStyleNavBar";
+
+const TimerNavBar = (props) => {
+  return <CardStyleNavBar {...props} />;
+};
+
+export default TimerNavBar;
--- src/components/utilServices/timer/TimerSection.jsx
+++ src/components/utilServices/timer/TimerSection.jsx
@@ -1,10 +1,37 @@
 import React from "react";
+import TimerNavBar from "./TimerNavBar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUndo, faCog } from "@fortawesome/free-solid-svg-icons";
 import "./TimerSection.css";
 
 const TimerSection = () => {
+  const timerButtons = [{ label: "Timer", value: "timer" }];
+
+  const infoButtons = [
+    { label: <FontAwesomeIcon icon={faUndo} />, value: "reset" },
+    { label: <FontAwesomeIcon icon={faCog} />, value: "settings" },
+  ];
+
+  // event handlers
+  const changeSelectedButton = (button) => {
+    console.log(button);
+  };
+
+  // helper methods for rendering
+  const renderTimerNavBar = () => {
+    return (
+      <TimerNavBar
+        leftSideButtons={timerButtons}
+        rightSideButtons={infoButtons}
+        activeButton={timerButtons[0]}
+        onButtonClick={changeSelectedButton}
+      />
+    );
+  };
+
   return (
     <div className="service-section timer-section">
-      <div className="card-style-content-area"></div>
+      <div className="card-style-content-area">{renderTimerNavBar()}</div>
     </div>
   );
 };
