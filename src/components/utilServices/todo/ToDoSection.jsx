5c5f6cbd22cb29663637551afe514e14df73a0b1
rabbykhairul <rabbykhairul@gmail.com>
2020-12-07 17:49:56 +0600

    render ToDoListDisplay component inside ToDoSection component

--- src/components/utilServices/todo/ToDoSection.jsx
+++ src/components/utilServices/todo/ToDoSection.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import ToDoListDisplay from "./ToDoListDisplay";
 import "./ToDoSection.css";
 
 const ToDoSection = () => {
-  return <div className="to-do-section">hello</div>;
+  return (
+    <div className="to-do-section">
+      <ToDoListDisplay />
+    </div>
+  );
 };
 
 export default ToDoSection;
