e7d8997c4c39a48166a63492beccda7efb2147ee
rabbykhairul <rabbykhairul@gmail.com>
2020-12-07 18:01:19 +0600

    add logic to check whether user is trying to create a new task or not

--- src/components/utilServices/todo/ToDoListDisplay.jsx
+++ src/components/utilServices/todo/ToDoListDisplay.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ToDoListDisplay.css";
 
 const ToDoListDisplay = () => {
+  const [creatingNewTaskStatus, setCreatingNewTaskStatus] = useState(false);
+
+  // event handlers
+  const letUserCreateANewTask = () => {
+    setCreatingNewTaskStatus(true);
+  };
+  // helper methods for rendering
   const renderListHeader = () => {
     return <h1 className="to-do-list-main-header">Today</h1>;
   };
 
   const renderAddTaskPrompt = () => {
+    if (creatingNewTaskStatus) return null;
+
     return (
-      <div className="add-task-button">
+      <div className="add-task-button" onClick={letUserCreateANewTask}>
         <span className="plus-icon">+</span>
         Add task
       </div>
