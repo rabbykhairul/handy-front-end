d9ca3d5f20ff26f2f2dc81c198c775426a870aa1
rabbykhairul <rabbykhairul@gmail.com>
2020-12-07 21:40:06 +0600

    render text input area inside ToDoItemForm

--- src/components/utilServices/todo/ToDoItemForm.jsx
+++ src/components/utilServices/todo/ToDoItemForm.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import ToDoTextAreaInput from "./ToDoTextAreaInput";
 import "./ToDoItemForm.css";
 
 const ToDoItemForm = () => {
-  return <div className="to-do-item-form" tabIndex="0"></div>;
+  const [toDoDescription, setToDoDescription] = useState("");
+
+  const updateToDoDescription = (newDescription) => {
+    setToDoDescription(newDescription);
+  };
+
+  return (
+    <div className="to-do-item-form" tabIndex="0">
+      <ToDoTextAreaInput
+        name="toDoDescription"
+        value={toDoDescription}
+        placeholder="e.g. renew neteflix subscription"
+        onChange={updateToDoDescription}
+      />
+    </div>
+  );
 };
 
 export default ToDoItemForm;
