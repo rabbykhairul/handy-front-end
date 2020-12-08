4bc931f01f61f412a2a79911b0c31f74e822d0aa
rabbykhairul <rabbykhairul@gmail.com>
2020-12-08 15:53:48 +0600

    show completed task in todo list with a different style

--- src/components/utilServices/todo/ToDoItem.jsx
+++ src/components/utilServices/todo/ToDoItem.jsx
@@ -6,13 +6,16 @@ import "./ToDoItem.css";
 const ToDoItem = (props) => {
   const { description, checked, onCheck } = props;
   const checkIcon = checked ? faCheckCircle : faCircle;
+  const descriptionTextClassName = checked
+    ? "to-do-description checked"
+    : "to-do-description";
 
   return (
     <div className="to-do-item">
       <div className="check-button" onClick={onCheck}>
         <FontAwesomeIcon icon={checkIcon} />
       </div>
-      <div className="to-do-description">{description}</div>
+      <div className={descriptionTextClassName}>{description}</div>
     </div>
   );
 };
