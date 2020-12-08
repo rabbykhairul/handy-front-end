5ac37b08a49b6da22d38724ef4e47ffdb3e118ff
rabbykhairul <rabbykhairul@gmail.com>
2020-12-08 09:50:11 +0600

    render ToDoFormButton with different style based on props

--- src/components/utilServices/todo/ToDoFormButton.jsx
+++ src/components/utilServices/todo/ToDoFormButton.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./ToDoFormButton.css";
 
-const ToDoFormButton = () => {
-  return <button className="to-do-form-button btn-fancy">Add task</button>;
+const ToDoFormButton = (props) => {
+  const { label, styleType = "normal" } = props;
+
+  const className =
+    styleType.toLowerCase() === "fancy"
+      ? "to-do-form-button btn-fancy"
+      : "to-do-form-button btn-normal";
+
+  return <button className={className}>{label}</button>;
 };
 
 export default ToDoFormButton;
