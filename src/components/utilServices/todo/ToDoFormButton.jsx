4b485513bb2892557263f3c8b107f32963b80ed8
rabbykhairul <rabbykhairul@gmail.com>
2020-12-08 10:06:46 +0600

    apply disabled attribute and style to ToDoFormButton when applicable

--- src/components/utilServices/todo/ToDoFormButton.jsx
+++ src/components/utilServices/todo/ToDoFormButton.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import "./ToDoFormButton.css";
 
 const ToDoFormButton = (props) => {
-  const { label, styleType = "normal" } = props;
+  const { label, styleType = "normal", disabled = false } = props;
 
   const className =
     styleType.toLowerCase() === "fancy"
       ? "to-do-form-button btn-fancy"
       : "to-do-form-button btn-normal";
 
-  return <button className={className}>{label}</button>;
+  return (
+    <button className={className} disabled={disabled}>
+      {label}
+    </button>
+  );
 };
 
 export default ToDoFormButton;
