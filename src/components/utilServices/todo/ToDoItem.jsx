7ba113f9f6a56cdd0d70606ff277596693919e09
rabbykhairul <rabbykhairul@gmail.com>
2020-12-08 13:38:50 +0600

    render todo description dynamically inside ToDoItem component

--- src/components/utilServices/todo/ToDoItem.jsx
+++ src/components/utilServices/todo/ToDoItem.jsx
@@ -3,13 +3,15 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircle } from "@fortawesome/free-regular-svg-icons";
 import "./ToDoItem.css";
 
-const ToDoItem = () => {
+const ToDoItem = (props) => {
+  const { description } = props;
+
   return (
     <div className="to-do-item">
       <div className="check-button">
         <FontAwesomeIcon icon={faCircle} />
       </div>
-      <div className="to-do-description">Renew neteflix subscription</div>
+      <div className="to-do-description">{description}</div>
     </div>
   );
 };
