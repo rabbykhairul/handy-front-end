dc8f4c05347250c50b2b0a34e49f2db201badd18
rabbykhairul <rabbykhairul@gmail.com>
2020-12-08 09:36:54 +0600

    create ToDoFormButton component

---
+++ src/components/utilServices/todo/ToDoFormButton.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ToDoFormButton.css";
+
+const ToDoFormButton = () => {
+  return <button className="to-do-form-button btn-fancy">Add task</button>;
+};
+
+export default ToDoFormButton;
