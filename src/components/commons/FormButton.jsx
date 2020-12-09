9a9b40bb2428f93ca62322fd77be235b00a9b775
rabbykhairul <rabbykhairul@gmail.com>
2020-12-10 02:32:06 +0600

    Modify the FormButton component to show a message on form submission

--- src/components/commons/FormButton.jsx
+++ src/components/commons/FormButton.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faYinYang } from "@fortawesome/free-solid-svg-icons";
 import "./FormButton.css";
 
 const FormButton = (props) => {
-  const { label } = props;
+  const { label, submitStatus = false, submitMessage } = props;
 
-  return <button className="form-button">{label}</button>;
+  if (!submitStatus) return <button className="form-button">{label}</button>;
+
+  // if the user has submitted the form show animation while the form is being processed
+  return (
+    <div className="form-button form-button-submit-animation">
+      <FontAwesomeIcon icon={faYinYang} spin />
+      {` `} {submitMessage}
+    </div>
+  );
 };
 
 export default FormButton;
